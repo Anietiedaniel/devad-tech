a6021df575f4513f3cbb98540eca8cb3ccc6523f
AnietieDaniel <anietienteabasi@gmail.com>
2026-04-30 23:53:45 +0100

    first

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import HeroSection from "../sections/homeSections/HeroSection";
 import HomeAuthoritySection from "../sections/homeSections/HomeAuthoritySection";
 import HomeProcessSection from "../sections/homeSections/Process";
 import HomeFeaturedProjects from "../sections/homeSections/Projects"
-import HomeCTASection from "../sections/homeSections/HomeActionSection";
+import HomeActionSection from "../sections/homeSections/HomeActionSection";
 import HomeTestimonialSection from "../sections/homeSections/Testimonial";
 
 export default function Home() {
