cdce7e5f42a5425a1f79db227c3f315356e82d3c
AnietieDaniel <anietienteabasi@gmail.com>
2026-06-12 17:48:50 +0100

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
 // Pull your Google Client ID dynamically from environment variables
-const googleClientId =38292348393-5dvtc0vt03gc8jmdm2av1on0bi895sr3.apps.googleusercontent.com
+const googleClientId="38292348393-5dvtc0vt03gc8jmdm2av1on0bi895sr3.apps.googleusercontent.com"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
